deb180b2f09255a67353be8f8b0a9d7db1549229
Fabíola <fabiolasoufer@gmail.com>
2022-12-15 07:50:22 -0300

    @fabiolasoufer Task: Evitando queries N + 1

---
+++ src/main/java/TestaListagemDeCategorias.java
@@ -0,0 +1,25 @@
+import br.com.alura.jdbc.ConnectionFactory;
+import dao.CategoriaDAO;
+import model.Categoria;
+import model.Produto;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TestaListagemDeCategorias {
+
+    public static void main(String[] args) throws SQLException {
+
+        try(Connection connection = new ConnectionFactory().recuperarConexao()) {
+            CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
+            List<Categoria> listaDeCategorias = categoriaDAO.listarComProdutos();
+            listaDeCategorias.stream().forEach(ct -> {
+                System.out.println(ct.getNome());
+                for (Produto produto : ct.getProdutos()) {
+                    System.out.println(ct.getNome() + " - " + produto.getNome());
+                }
+            });
+        }
+    }
+}
--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import model.Categoria;
 import model.Produto;
 
 import java.sql.*;
@@ -38,6 +39,29 @@ public class ProdutoDAO {
         String sql = "SELECT ID, NOME, DESCRICAO FROM produto";
 
         try(PreparedStatement pstm = connection.prepareStatement(sql)) {
+            pstm.execute();
+
+            try(ResultSet rst = pstm.getResultSet()) {
+                while(rst.next()) {
+                    Produto produto =
+                            new Produto(rst.getInt(1), rst.getString(2), rst.getString(3));
+
+                    produtos.add(produto);
+                }
+            }
+        }
+        return produtos;
+    }
+
+    public List<Produto> buscar(Categoria ct) throws SQLException {
+        List<Produto> produtos = new ArrayList<Produto>();
+
+        System.out.println("Executando a query de buscar produto");
+
+        String sql = "SELECT ID, NOME, DESCRICAO FROM produto WHERE categoria_id = ?";
+
+        try(PreparedStatement pstm = connection.prepareStatement(sql)) {
+            pstm.setInt(1, ct.getId());
             pstm.execute();
 
             try(ResultSet rst = pstm.getResultSet()) {
